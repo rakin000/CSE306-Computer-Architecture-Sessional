221b10ba85bdbd6361486c35db2fa729802f5536
quvi007 <1805007@ugrad.cse.buet.ac.bd>
2022-08-28 17:39:53 +0600

    without mux alu, dm

---
+++ Offline4/ALU_without_mux/ALU_without_mux/main.c
@@ -0,0 +1,44 @@
+#include <avr/io.h>
+ 
+ 
+int main(void)
+{
+        DDRB = 0x00;
+        DDRD = 0x00;
+        DDRA = 0xFF;
+        DDRC = 0xFF;
+       
+    while (1)
+    {
+                unsigned char op1 = (PINB >> 4);
+                unsigned char op2 = (PINB & 0x0F);
+               // unsigned char immediate = (PIND >> 4);
+               // unsigned char alusrc = (PIND >> 3) & 0x01;	
+                unsigned char aluop = (PIND & 0x07);
+               
+                unsigned char result=0;
+               
+                
+                if (aluop == 0)
+					result = op1 + op2;
+                else if (aluop == 1)
+					result = (op1 << op2);
+                else if (aluop == 2)
+					result = op1 - op2;
+                else if (aluop == 3)
+					result = op1 | op2;
+                else if (aluop == 4)
+					result = op1 & op2;
+                else if (aluop == 5)
+					result = (op1 >> op2);
+                else if (aluop == 6)
+					result = ~(op1 | op2);
+              
+               
+                PORTA = ((result)<<4);
+				PORTA &= (0x01 << 3) ;
+                if ((result & 0x0F) == 0) {
+                        PORTA |= (0x01 << 3);
+                }
+    }
+}
---
+++ Offline4/DataMemory_without_mux/DataMemory_without_mux/main.c
@@ -0,0 +1,50 @@
+#include <avr/io.h>
+
+int main(void)
+{
+	MCUCSR = (1<<JTD);
+	MCUCSR = (1<<JTD) ;
+	DDRB = 0x00;	// B4-7 = address
+					// B0-3 = write_data
+	DDRD = 0x00 ;	// D0 = MEM_READ
+					// D1 = MEM_WRITE
+					// D2 = MEM_TO_REG
+					// D7 = CLK
+	DDRA = 0xFF ;
+	DDRC = 0xFE ;
+	PORTA=0x01;
+	const int N = 1<<8;
+	unsigned char data[N] ;
+	for (int i=0; i<N; i++)
+		data[i] = 0;
+	//data[0] = 0b0000000000000000;
+	//data[1] = 0b1100101101001010;
+	//data[2] = 0b1111111100000000;
+	//data[3] = 0b0000000011111111;
+	//data[4] = 0b0000111100001111;
+	
+	unsigned char in,location,write_data,memwrite,read_data;
+	int prevclk = 0, clk;
+	while (1){
+		in = PINB;
+		write_data = (unsigned char)(in & 0x0F) ;
+		location = (unsigned char)( (in >> 4 ) & 0x0F );
+		
+		memwrite = (PIND >> 1) & 1;
+		// mem_to_reg = (PIND >> 2) & 1;
+		clk = (PIND>>7) & 1;
+		
+		if(memwrite==1)
+		{
+			if (prevclk == 0 && clk == 1){
+				data[location]=write_data;
+			}
+		}
+		
+		
+		read_data=data[location];
+		
+		PORTA = read_data;
+		prevclk = clk;
+	}
+}
